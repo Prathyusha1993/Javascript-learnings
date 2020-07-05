62151280702c4fa5df1d3a32ddc3467f30ab826e
Prathyusha13 <pkr131023@gmail.com>
2020-07-04 17:25:15 -0700

    second commit

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-alert("hello world");
+/*alert("hello world");
   alert('comes as pop up');
+  console.log("any message");*/
+
+//   let total = 49.99;
+//   let product = "hikng boots";
+//   let discounted = true;
+//   showMessage(total);
+
+let total = 189.99, //declaring a variable
+    product = 'hiking boots', //declaring a string
+    discounted = true; 
+showMessage(product );
